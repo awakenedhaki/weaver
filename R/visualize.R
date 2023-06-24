659097f3779382bae49059e6f73e0420ce82ce03
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-06-23 22:22:08 -0700

    Adding roxygen2 documentation

--- R/visualize.R
+++ R/visualize.R
@@ -1,3 +1,24 @@
+#' Plot Graph
+#'
+#' This function plots a graph using the ggraph package.
+#' It performs the following steps:
+#' - Decomposes the input graph into subgraphs based on the specified minimum number of vertices using `decompose()`.
+#' - Binds the subgraphs together into a single graph using the `bind_subgraphs()` function.
+#' - Optionally removes bridges from the graph using the `bridge_remover()` function.
+#' - Plots the resulting graph using the Fruchterman-Reingold layout in ggraph.
+#' - Uses alpha blending of edges based on the index attribute.
+#' - Colors nodes as light grey with a larger size for the subgraph nodes, and black with a smaller size for other nodes.
+#' - Applies a void theme to the plot and removes the legend.
+#'
+#' @param graph The input graph.
+#' @param remove_bridges Logical indicating whether to remove bridges from the graph (default is FALSE).
+#' @param min_vertices The minimum number of vertices required for subgraph decomposition (default is 3).
+#'
+#' @import igraph
+#' @importFrom ggraph ggraph geom_edge_link geom_node_point
+#' @importFrom ggplot2 aes theme_void theme
+#'
+#' @export
 plot_graph <- function(graph, remove_bridges = FALSE, min_vertices = 3) {
   graph <- igraph::decompose(graph, min.vertices = min_vertices) |>
     bind_subgraphs()
